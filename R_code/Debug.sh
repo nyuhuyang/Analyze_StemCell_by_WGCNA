e9e3459dc835c9a88e6f0e1dbde8852713ce5ffd
Yang Hu <nyuhuyang@gmail.com>
2017-06-26 22:47:55 -0400

    Add files via upload

---
+++ R_code/Debug.sh
@@ -0,0 +1,14 @@
+//
+//  Debug.sh
+//  
+//
+//  Created by Yang Hu on 2017/6/20.
+//
+//
+
+#include <stdio.h>
+
+sft = pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+#Error in datk[c(startG:endG), ] = foreach(t = actualThreads, .combine = rbind) %dopar%  :
+#number of items to replace is not a multiple of replacement length
+#"The code below uses parallel computation where multiple cores are available. This works well when R is run from a terminal or from the Graphical User Interface (GUI) shipped with R itself, but at present it does not work with RStudio and possibly other third-party R environments." -WGCNA package tutorial
