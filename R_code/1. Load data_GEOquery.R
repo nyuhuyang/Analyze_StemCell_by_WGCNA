a6f02dd39b6e8a6c4ce70d9d8c9bd57d22c902ba
Yang Hu <nyuhuyang@gmail.com>
2017-07-10 22:58:59 -0400

    Add files via upload

--- R_code/1. Load data_GEOquery.R
+++ R_code/1. Load data_GEOquery.R
@@ -5,11 +5,11 @@
 #    - by first computing modules (using WCGNA) in each dataset,
 #        then comparing overlap of modules using hypergeometric test
 
-############################################################################
-# 
+########################################################################
+#
 #  0 check and install all cran and bioconductor packages if necessary
 # 
-# =====================================================================================
+# ######################################################################
 
 list.of.cran.packages<- c("WGCNA","easypackages","stringr",
                           "dplyr","pheatmap","gridExtra")
@@ -27,12 +27,12 @@ if(length(new.packages)) biocLite(new.packages)
 libraries(list.of.bio.packages,list.of.cran.packages)
 memory.limit(size=25000)
 
-############################################################################
+# #####################################################################
 # 
 #  1.a Load microarray data---stem cell stages
 #  https://kasperdanielhansen.github.io/genbioconductor/html/GEOquery.html (coursera)
-# =====================================================================================
-
+#
+# ####################################################################
 
 # ===========load processed data (essential)============================================
 eList <- getGEO("GSE29397") #it takes some time
@@ -43,10 +43,11 @@ eData
 names(pData(eData))#there is usually a lot of unnecessary stuff here
 pData(eData)$title #enough cell information from title
 sampleNames(eData) <-pData(eData)$title
+par(mfrow=c(1,1))
 boxplot(eData)
 #normData <- rma(rawData) #  already normalized, doesn't work 
 #normData
-############################################################################
+#===========================================================================#
 
 
 # ------Loading raw data (alternative)-----------------------------------------------------------
@@ -129,7 +130,14 @@ names(rnaseqData);
 # -----------------------------------------------------------------
 
 
-##################Loading annotation data (essential)---stem cell stages############
+# #####################################################################
+# 
+#  1.b prepare microarray data---stem cell stages
+#  (home made)
+#
+# ####################################################################
+
+#==================Loading annotation data (essential)---stem cell stages============
 annotation(eData)
 platf <- getGEO(annotation(eData), AnnotGPL=TRUE)
 anot <- data.frame(attr(dataTable(platf), "table"))
@@ -137,7 +145,7 @@ anot1<- str_split_fixed(anot[,"Gene.symbol"], "///",2) #split column 10
 anno<-cbind.data.frame(anot[,"ID"],anot1[,1])
 anno <- anno[!anno[,2]=="",] #remove empty columns with no gene names
 colnames(anno) <-c("PROBES","gene_ID")
-############################################################################
+#===========================================================================#
 
 
 #  ----------Loading annotation data (locally,alternative)---stem cell stages----------
@@ -160,9 +168,9 @@ colnames(anno) <-c("PROBES","gene_ID")
 # -----------------------------------------------------------------
 
 
-################replace affymetrix ID to gene ID (essential)####################
-PROBES <- as.numeric(rownames(MicroArrayData))
-MicroArrayData<- cbind.data.frame(PROBES,MicroArrayData)
+#===============#replace affymetrix ID to gene ID (essential)==================##
+PROBES <- as.numeric(rownames(exprs(eData)))
+MicroArrayData<- cbind.data.frame(PROBES,exprs(eData))
 
 anno_MicroArrayData <- merge(anno, MicroArrayData, all = TRUE)
 anno_MicroArrayData <- anno_MicroArrayData[!is.na(anno_MicroArrayData[,3]),] #remove NA row in exprs
@@ -183,21 +191,27 @@ anno_MicroArrayData <- anno_MicroArrayData[,-1] #remove "PROBES"
 sd = apply(anno_MicroArrayData[,-1], 1, sd) #run sd w/o gene_ID
 sd = sd[order(sd, decreasing=TRUE)]
 Top_Std_gene <-names(sd)[1:(length(sd)*0.25)]
-# ############################################################################
+# ===========================================================================#
 
 
 # ------------visulization (optional)-------------------------------------------------------------
-sd =    apply(anno_MicroArrayData[,-1], 1, sd) #run sd w/o gene_ID
 median  = apply(anno_MicroArrayData[,-1], 1, median)
 par(mfrow=c(1,1))
 plot(median,sd,xlab="log2(count size)", ylab="standard deviation")
 lines(lowess(median,sd), col="blue")# lowess line (x,y)
-# ############################################################################
+
+
+
+
+
+
+
+# ######################################################################
 # 
-#  1.b loading Expression data---rnaseq_rowcounts
+#  1.c loading Expression data---rnaseq_rowcounts
 #  consult to http://combine-australia.github.io/RNAseq-R/06-rnaseq-day1.html
 #
-# =============================================================================
+# ######################################################################
 
 #======================Reading in the data (essential)======================
 # Display the current working directory
@@ -217,54 +231,106 @@ countdata<-countdata[,-1]
 # Obtain CPMs
 myCPM <- cpm(countdata)
 # Which values in myCPM are greater than 0.5?
-thresh <- myCPM > 0.5
-table(rowSums(thresh))
+thresh <- myCPM > 1
+table(rowSums(thresh) >= 3)
 # we would like to keep genes that have at least 2 TRUES in each row of thresh
-keep <- rowSums(thresh) >= 2
+keep <- rowSums(thresh) >= 3
 # Subset the rows of countdata to keep the more highly expressed genes
 myCPM.keep <- myCPM[keep,]
 
-#===========DEG visulization (essential)=================================
-# Library sizes and distribution plots
-expr1 <- DGEList(myCPM.keep)
-# Get log2 counts per million
-logcounts2 <- cpm(expr1,log=TRUE)
-############################################################################
-
-
-
-# ------------visulization (optional)-------------------------------------------------------------
-# Library sizes and distribution plots
+#creat DGE------------------------
 expr0 <- DGEList(countdata)
+group <- as.factor(c("E8","E8","E8","PGL","PGL","PGL","SCIL","SCIL","SCIL"))
+expr0$samples$group <- group
 
+expr1 <- expr0
+expr1$counts <-myCPM.keep
 
-counts0 <- cpm(expr0,log=FALSE)
+# Get log2 counts per million
+logcounts1 <- cpm(expr0,log=TRUE)
 logcounts2 <- cpm(expr1,log=TRUE)
+#normalising gene expression distributions
+expr1 <- calcNormFactors(expr1, method ="TMM")
+expr1$samples
+logcounts3 <- cpm(expr1,log=TRUE)
+#===========================================================================#
 
-#check the low reads range
-par(mfrow=c(1,3))
+
+
+# ######################################################################
+# 
+#  1.d prepare Expression data---rnaseq_rowcounts
+#  consult to Law c 's F1000Research 2016 http://dx.doi.org/10.12688/f1000research.9005.1
+#
+# ######################################################################
+
+#===========plot for the low reads range===========
+par(mfrow=c(1,2))
 par(oma=c(2,2,2,2))
 
-plot(exprs(eData),cex=.5)
+#
+#plot(counts0,cex=.5)
 plot(logcounts1[,1:2], cex=.5)
-plot(logcounts2[,1:2], cex=.5)
+title("raw data")
+plot(logcounts3[,1:2], cex=.5) 
+title("filterd data")
 
+
+#===========density polt===========
+library(RColorBrewer)
+nsamples <- ncol(logcounts2)
+col <- brewer.pal(nsamples, "Paired")
+par(mfrow=c(1,2),cex=1.3)
+plot(density(logcounts1[,1]), col=col[1], lwd=2, ylim=c(0,0.4), las=2,
+     main="", xlab="")
+title(main="A. Raw data", xlab="Log-cpm")
+abline(v=0, lty=3)
+for (i in 2:nsamples){
+        den <- density(logcounts1[,i])
+        lines(den$x, den$y, col=col[i], lwd=2)
+}
+legend("topright", colnames(logcounts1), text.col=col, bty="n")
+
+
+plot(density(logcounts3[,1]), col=col[1], lwd=2, ylim=c(0,0.21), las=2,
+     main="", xlab="")
+title(main="B. Filtered data", xlab="Log-cpm")
+abline(v=0, lty=3)
+for (i in 2:nsamples){
+        den <- density(logcounts2[,i])
+        lines(den$x, den$y, col=col[i], lwd=2)
+}
+legend("topright", colnames(logcounts2), text.col=col, bty="n")
+
+#===========boxplot (raw vs normalised)===========
 par(mfrow=c(1,3))
 par(oma=c(5,2,2,2))
-# Check distributions of samples using boxplots
+#distributions of samples using boxplots
 # The las argument rotates the axis names
-boxplot(counts0, xlab="", ylab="counts per million",las=2) # test with all((counts0)>=0)
-abline(h=median(counts0),col="blue")
-title("CPMs (unnormalised)")
-
 
 boxplot(logcounts1, xlab="", ylab="Log2 counts per million",las=2) # test with all((logcounts1+5.2)>0)
 abline(h=median(logcounts1),col="blue")
-title("logCPMs (unnormalised)")
+title("logCPMs (raw)")
 
 boxplot(logcounts2, xlab="", ylab="Log2 counts per million",las=2) # test with all((logcounts2+2)>0)
 abline(h=median(logcounts2),col="blue")
+title("logCPMs (trimed,un-normalisd)")
+
+
+boxplot(logcounts3, xlab="", ylab="Log2 counts per million",las=2) # test with all((logcounts2+2)>0)
+abline(h=median(logcounts2),col="blue")
 title("logCPMs (normalised)")
+
+#===========Unsupervised clustering of samples===========
+par(mfrow=c(1,2))
+col.group <- group
+levels(col.group) <- brewer.pal(nlevels(col.group), "Set1")
+col.group <- as.character(col.group)
+plotMDS(logcounts2, labels=group, col=col.group)
+title(main="A. Sample groups")
+plotMDS(logcounts3, labels=group, col=col.group)
+title(main="B. Sample groups")
+
 #-----Visulaze Mean-variance relationships (optional)-----------------
 sd      = apply(logcounts2, 1, sd)
 median  = apply(logcounts2, 1, median)
@@ -272,15 +338,16 @@ par(mfrow=c(1,1))
 
 plot(median,sd,xlab="log2(count size)", ylab="standard deviation")
 lines(lowess(median,sd), col="blue")# lowess line (x,y)
-############################################################################
+
+######################################################################
 # 
-#  1.c merge rnaseq with MicroArrayData
+#  1.e merge rnaseq with MicroArrayData
 #  (home made)
-#=============================================================================
+#######################################################################
 
 #=============    merge 1.a and 1.b (essential)======================
 #put gene column back-
-logcounts <- cbind.data.frame(rownames(logcounts2),logcounts2) 
+logcounts <- cbind.data.frame(rownames(logcounts3),logcounts3) 
 colnames(logcounts)[1] <- "gene_ID"
 
 
@@ -291,22 +358,48 @@ rnaseq_MicroArrayData <- rnaseq_MicroArrayData[,-1] #remove "gene_ID"
 write.csv(rnaseq_MicroArrayData,"rnaseq_MicroArrayData.csv")
 
 
-par(mfrow=c(1,2))
-par(oma=c(16,2,0,2), cex=1.2)
-boxplot(rnaseq_MicroArrayData_std, xlab="", ylab="log2 counts per million",
-        main = "boxplot for log Counts Per Million of all samples",las=2) # test with all((counts0)>=0)
 
 
 #===========get gene list that have higher std (top 25%) (essential)===========
 all_gene <- rownames(rnaseq_MicroArrayData)
-all_gene %in% Top_Std_gene
 rnaseq_MicroArrayData_std <-rnaseq_MicroArrayData[all_gene %in% Top_Std_gene,]
+
+par(mfrow=c(1,1))
+par(oma=c(16,2,0,2), cex=1.2)
+boxplot(rnaseq_MicroArrayData_std, xlab="", ylab="log2 counts per million",
+        main = "boxplot for log Counts Per Million of all samples",las=2) # test with all((counts0)>=0)
+
 #rnaseq_MicroArrayData[Top_Std_gene,] will creat NA
 
 dim(rnaseq_MicroArrayData_std)
 write.csv(rnaseq_MicroArrayData_std,"rnaseq_MicroArrayData_std.csv")
-############################################################################
+#==========================================================================#
 
+######################################################################
+# 
+#  1.e merge rnaseq with MicroArrayData
+#  (home made)
+
+#======Visulaze Mean-variance relationships (optional)===========
+par(mfrow=c(1,2))
+sd1      = apply(rnaseq_MicroArrayData[,10:ncol(rnaseq_MicroArrayData_std)], 1, sd)
+median1  = apply(rnaseq_MicroArrayData[,10:ncol(rnaseq_MicroArrayData_std)], 1, median)
+
+sd2      = apply(rnaseq_MicroArrayData[,1:9], 1, sd)
+median2  = apply(rnaseq_MicroArrayData[,1:9], 1, median)
+
+
+plot(median1,sd1,xlab="log2(count size)", ylab="standard deviation",
+     main = "Mean-variance relationships for Mircroarray",las=2)
+lines(lowess(median1,sd1), col="blue")# lowess line (x,y)
+abline(h=median(sd1[all_gene %in% Top_Std_gene]),col="red")
+
+
+
+plot(median2,sd2,xlab="log2(count size)", ylab="",
+     main = "Mean-variance relationships for RNA-seq",las=2)
+lines(lowess(median2,sd2), col="blue")# lowess line (x,y)
+abline(h=median(sd2[all_gene %in% Top_Std_gene]),col="red")
 
 
 #----find out highly expression genes (optional)-----------
@@ -318,3 +411,10 @@ par(oma=c(2,2,2,2), cex=1.3)
 boxplot(t(rnaseq_MicroArrayData[Top_expressed_genes[1:20],1:9]),
         ylab="log2 counts per million",main = "Top 15 highly expressed genes in RNA-seq data",las=2)
 
+# ------------visulization filteration (optional)-------------------------------------------------------------
+#check the low reads range
+par(mfrow=c(1,2))
+par(oma=c(2,2,2,2))
+
+plot(rnaseq_MicroArrayData[,1:2],cex=.5)
+plot(rnaseq_MicroArrayData_std[,1:2],cex=.5)
